ceb51e6105a9250f1df25dcb7411cde7ebc5d44f
Hugo Borsier <h_borsie@live.concordia.ca>
2017-12-06 11:59:20 -0500

    yolo

---
+++ shop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index page</title>
+	<meta charset="utf-8">
+	<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Welcome to the shop</h1>
+	<ul>
+		<li>Please register <a href="register.php">here</a></li>
+		<li>Please login <a href="login.php">here</a></li>
+	</ul>
+</body>
+</html>
